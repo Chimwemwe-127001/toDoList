9462da590cd39165053828c861b8d543ad5dfa44
Neolicious <52143815+Chimwemwe-127001@users.noreply.github.com>
2021-11-04 20:47:57 +0200

    Remove messsy comments from code as advised by code reviewer

--- src/__tests__/todo.test.js
+++ src/__tests__/todo.test.js
@@ -20,29 +20,6 @@ document.body.innerHTML = `
 
 const addItemField = document.getElementById('input');
 
-// function storageMock() {
-//   const storage = {};
-
-//   return {
-//     setItem(key, value) {
-//       storage[key] = value || '';
-//     },
-//     getItem(key) {
-//       return key in storage ? storage[key] : null;
-//     },
-//     removeItem(key) {
-//       delete storage[key];
-//     },
-//     get length() {
-//       return Object.keys(storage).length;
-//     },
-//     key(i) {
-//       const keys = Object.keys(storage);
-//       return keys[i] || null;
-//     },
-//   };
-// }
-
 describe('#addToList is working properly', () => {
   // Arrange
   const todolist = new List();
